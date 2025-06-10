f730c9cf88a63e32e094f9d1a389c6f32d4a5ffe
Mark <hendriksen-mark@hotmail.com>
2025-06-10 14:19:20 +0200

    add test

---
+++ src/index.spec.ts
@@ -0,0 +1,20 @@
+import plugin from './index';
+
+describe('homebridge-http-curtain plugin', () => {
+  it('should export a function', () => {
+    expect(typeof plugin).toBe('function');
+  });
+
+  it('should register accessory when called with mock API', () => {
+    const mockRegisterAccessory = jest.fn();
+    const mockApi = {
+      hap: {
+        Service: {},
+        Characteristic: {},
+      },
+      registerAccessory: mockRegisterAccessory,
+    };
+    plugin(mockApi as any);
+    expect(mockRegisterAccessory).toHaveBeenCalled();
+  });
+});
